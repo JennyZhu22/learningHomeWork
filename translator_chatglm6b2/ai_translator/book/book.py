25409f5071e03fade8b362f19cd37615a2fb5173
JennyZhu22 <118274345+JennyZhu22@users.noreply.github.com>
2023-09-17 16:43:54 +0800

    Add files via upload

---
+++ translator_chatglm6b2/ai_translator/book/book.py
@@ -0,0 +1,11 @@
+from .page import Page
+
+class Book:
+    def __init__(self, pdf_file_path):
+        print(" file path in book ->")
+        print(pdf_file_path)
+        self.pdf_file_path = pdf_file_path
+        self.pages = []
+
+    def add_page(self, page: Page):
+        self.pages.append(page)
